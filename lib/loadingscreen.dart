0e91ebe805b91e23f266322c5bddb649a70cd7d9
Brandon <bocanegrabb21@gmail.com>
2025-11-14 17:51:42 -0500

    this is the loading screen changes

--- lib/loadingscreen.dart
+++ lib/loadingscreen.dart
@@ -5,7 +5,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
 }
 
 class OnboardingScreen extends StatelessWidget {
-  const OnboardingScreen({Key? key}) : super(key: key);
+  const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
